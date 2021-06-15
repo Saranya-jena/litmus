984114f72c6c0e03edf341510e6bd093e645f212
Vansh Bhatia <vansh@chaosnative.com>
2021-06-15 12:43:58 +0530

    Merge conflicts resolved + heatmap WIP
    Signed-off-by: Vansh Bhatia <vansh@chaosnative.com>

--- litmus-portal/frontend/src/pages/WorkflowAnalytics/styles.ts
+++ litmus-portal/frontend/src/pages/WorkflowAnalytics/styles.ts
@@ -2,56 +2,36 @@ import { makeStyles } from '@material-ui/core/styles';
 
 // Component styles
 const useStyles = makeStyles((theme) => ({
-  rootContainer: {
-    height: '100%',
+  headingSection: {
     display: 'flex',
-    flexDirection: 'column',
-    justifyContent: 'space-between',
-    overflowX: 'hidden',
   },
-
-  popOverRootContainer: {
-    height: '20%',
-    display: 'flex',
-    flexDirection: 'column',
-    justifyContent: 'space-between',
+  pageHeading: {
+    flexGrow: 1,
   },
-
-  popOverRoot: {
-    marginTop: theme.spacing(5),
-    marginLeft: theme.spacing(6),
-    marginBottom: theme.spacing(8),
-  },
-
-  root: {
-    marginLeft: theme.spacing(1),
-    marginBottom: theme.spacing(8),
-  },
-
-  headerDiv: {
-    display: 'flex',
-    flexDirection: 'row',
-    marginTop: theme.spacing(3),
-  },
-
-  analyticsDiv: {
-    marginTop: theme.spacing(5),
-  },
-
-  waitingText: {
+  heading: {
     fontSize: '2rem',
-    marginLeft: '30%',
-    marginTop: '20%',
-    marginBottom: '10%',
+    marginBottom: theme.spacing(0.875),
   },
-
-  waitingScreen: {
-    alignContent: 'center',
+  subHeading: {
+    fontSize: '1rem',
+    marginBottom: theme.spacing(4.125),
   },
-
-  button: {
-    marginBottom: theme.spacing(3),
-    marginLeft: theme.spacing(-2),
+  infoStatsHeader: {
+    padding: theme.spacing(3.5),
+    width: '100%',
+    backgroundColor: theme.palette.background.paper,
+    '& p': {
+      fontSize: '1.5rem',
+      fontWeight: '500',
+    },
+  },
+  infoStatsSection: {
+    backgroundColor: theme.palette.cards.header,
+    padding: theme.spacing(2.5, 3.25),
+  },
+  infoStats: {
+    backgroundColor: theme.palette.background.paper,
+    padding: theme.spacing(3, 3.875),
   },
 }));
 
